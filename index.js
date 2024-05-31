9fd4a9d9d3cc6c12a15923dcf47d898fe7cdab0f
mi@26041988 <mformithra@gmail.com>
2024-05-31 11:45:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log("Hello Javascript")
+
+// let a="Mithra";
+// let b=1234567890;
+// let c=true;
+
+// let obj={
+//     name:"Sara",
+//     age:10,
+//     parent:{
+//         father:"Deepak",
+//         mother:"Mithra"
+//     }
+// }
+// console.log(obj.parent);
+
+
+// for(key in obj)
+//     {
+//         console.log(obj[key])
+//     }
+
+//     let ary=["hello",111,true,12345789,{},[]]
+
+//     ary.forEach((x)=>
+//         {
+//             console.log(x);
+//         }
+//     )
+
+//     for(let x of ary)
+//         {
+//             console.log(ary[x]);
+//         }
+//     console.log(ary[1]);
+
+
+// function print(x)
+// {
+//     console.log(x);
+// };
+
+// print("hai");
+
+
+// const format=(x)=>
+//     {
+//         console.log(x);
+//     }
+
+//     format(x);
+
+    let btnval='';
+    function btn1()
+    {
+     btnval +="Button is clicked.";
+     document.getElementById('demo').innerHTML=btnval;
+    }
+
+    
